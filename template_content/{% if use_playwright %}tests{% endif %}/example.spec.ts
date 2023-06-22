56f76aa7d7b9eecfc9837cb3a76fea05c5f773ef
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2023-06-22 23:21:32 +0200

    chore: addressing pr comments

--- template_content/{% if use_playwright %}tests{% endif %}/example.spec.ts
+++ template_content/{% if use_playwright %}tests{% endif %}/example.spec.ts
@@ -1,5 +1,5 @@
-import { test, expect } from '@playwright/test'
-import { generateAccount } from 'algosdk'
+import { randomAccount } from '@algorandfoundation/algokit-utils'
+import { expect, test } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:5173/')
@@ -27,12 +27,12 @@ test('authentication and dummy payment transaction', async ({ page }) => {
   // 2. Must be able to send a dummy payment transaction
   await page.getByTestId('transactions-demo').click()
 
-  const dummyAccount = generateAccount()
+  const dummyAccount = randomAccount()
   await page.getByTestId('receiver-address').fill(dummyAccount.addr)
   await page.getByTestId('send-algo').click()
 
   // 3. Must be able to see a notification that the transaction was sent
-  const notificiation = await page.getByText('Transaction sent:')
-  await notificiation.waitFor()
-  expect(notificiation).toBeTruthy()
+  const notification = await page.getByText('Transaction sent:')
+  await notification.waitFor()
+  expect(notification).toBeTruthy()
 })
