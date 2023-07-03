741e58997089f8db867038a4fabb0eb8fce7e7ab
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2023-07-03 17:26:46 +0200

    feat: integrating latest use-wallet v2

--- tests_generated/test_all_default_parameters_on_netlify/src/hooks/useAlgoWalletProvider.ts
+++ tests_generated/test_all_default_parameters_on_netlify/src/hooks/useAlgoWalletProvider.ts
@@ -5,14 +5,14 @@ import algosdk from 'algosdk'
 import { useEffect } from 'react'
 
 type SupportedProviders = Partial<{
-  kmd: Promise<WalletClient | null>
-  pera: Promise<WalletClient | null> // ok
-  myalgo: Promise<WalletClient | null>
-  algosigner: Promise<WalletClient | null>
-  defly: Promise<WalletClient | null> // ok
-  exodus: Promise<WalletClient | null> // ok
-  walletconnect: Promise<WalletClient | null>
-  mnemonic: Promise<WalletClient | null>
+  kmd: WalletClient | null
+  pera: WalletClient | null // ok
+  myalgo: WalletClient | null
+  algosigner: WalletClient | null
+  defly: WalletClient | null // ok
+  exodus: WalletClient | null // ok
+  walletconnect: WalletClient | null
+  mnemonic: WalletClient | null
 }>
 
 let providerIds: PROVIDER_ID[] = []
@@ -21,20 +21,20 @@ if (import.meta.env.VITE_ALGOD_NETWORK === '') {
 } else {
   providerIds = [PROVIDER_ID.PERA, PROVIDER_ID.DEFLY, PROVIDER_ID.EXODUS]
 }
-const walletProviders: SupportedProviders = {}
+const walletProviders: Partial<SupportedProviders> = {}
 
 export function useAlgoWallet(context: { autoConnect: boolean; network: string; nodeServer: string; nodePort: string; nodeToken: string }) {
   const algodOptions = [context.nodeToken, context.nodeServer, context.nodePort] as AlgodClientOptions
 
   const network = context.network
 
-  providerIds.forEach((id) => {
-    if (id in walletProviders) {
+  providerIds.forEach(async (id) => {
+    if (Object.keys(walletProviders).includes(id)) {
       return
     } else {
       switch (id) {
         case PROVIDER_ID.PERA:
-          walletProviders[id] = pera.init({
+          walletProviders[id] = await pera.init({
             algosdkStatic: algosdk,
             clientStatic: PeraWalletConnect,
             clientOptions: {
@@ -44,25 +44,48 @@ export function useAlgoWallet(context: { autoConnect: boolean; network: string; 
             network: network,
           })
           break
+
         case PROVIDER_ID.DEFLY:
-          walletProviders[id] = defly.init({
+          walletProviders[id] = await defly.init({
             algosdkStatic: algosdk,
             clientStatic: DeflyWalletConnect,
             algodOptions: algodOptions,
             network: network,
           })
           break
+
         case PROVIDER_ID.EXODUS:
-          walletProviders[id] = exodus.init({
+          walletProviders[id] = await exodus.init({
             algosdkStatic: algosdk,
             algodOptions: algodOptions,
             network: network,
           })
           break
 
+        // // For WalletConnect, you need to provide your own clientStatic and clientOptions
+        // // Refer to https://github.com/TxnLab/use-wallet for integration instructions
+        // case PROVIDER_ID.WALLETCONNECT:
+        //   walletProviders[id] = await walletconnect.init({
+        //     algosdkStatic: algosdk,
+        //     clientStatic: WalletConnectModalSign,
+        //     clientOptions: {
+        //       relayUrl: 'wss://relay.walletconnect.com',
+        //       projectId: '{YOUR_PROJECT_ID}',
+        //       metadata: {
+        //         name: 'AlgoKit React Starter',
+        //         description: 'AlgoKit React Starter web app',
+        //         url: '#',
+        //         icons: ['https://algorand.com/assets/media-kit/logos/logo-marks/png/algorand_logo_mark_white.png'],
+        //       },
+        //     },
+        //     algodOptions: algodOptions,
+        //     network: network,
+        //   })
+        //   break
+
         default:
           if (import.meta.env.VITE_ALGOD_NETWORK === '') {
-            walletProviders[PROVIDER_ID.KMD] = kmd.init({
+            walletProviders[PROVIDER_ID.KMD] = await kmd.init({
               algosdkStatic: algosdk,
               algodOptions: algodOptions,
               network: network,
@@ -73,8 +96,12 @@ export function useAlgoWallet(context: { autoConnect: boolean; network: string; 
   })
 
   useEffect(() => {
+    const reconnect = async (walletProviders: SupportedProviders) => {
+      await reconnectProviders(walletProviders)
+    }
+
     if (context.autoConnect) {
-      reconnectProviders(walletProviders)
+      reconnect(walletProviders)
     }
   }, [])
 
