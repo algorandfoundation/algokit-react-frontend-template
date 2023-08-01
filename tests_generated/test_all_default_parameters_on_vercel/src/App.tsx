4a6fcaca9af832ada8e351751c4a028472f603ce
Al <aorumbayev@pm.me>
2023-08-01 17:36:44 +0200

    feat: use-wallet v2 integration, expanded guidelines, improvements in kmd support (#13)
    * refactor: changing name for kmd

--- tests_generated/test_all_default_parameters_on_vercel/src/App.tsx
+++ tests_generated/test_all_default_parameters_on_vercel/src/App.tsx
@@ -1,10 +1,39 @@
-import { WalletProvider, useWallet } from '@txnlab/use-wallet'
+import { DeflyWalletConnect } from '@blockshake/defly-connect'
+import { DaffiWalletConnect } from '@daffiwallet/connect'
+import { PeraWalletConnect } from '@perawallet/connect'
+import { PROVIDER_ID, ProvidersArray, WalletProvider, useInitializeProviders, useWallet } from '@txnlab/use-wallet'
+import algosdk from 'algosdk'
 import { SnackbarProvider } from 'notistack'
 import { useState } from 'react'
 import ConnectWallet from './components/ConnectWallet'
 import Transact from './components/Transact'
-import { useAlgoWallet } from './hooks/useAlgoWalletProvider'
-import { getAlgodConfigFromViteEnvironment } from './utils/network/getAlgoClientConfigs'
+import { getAlgodConfigFromViteEnvironment, getKmdConfigFromViteEnvironment } from './utils/network/getAlgoClientConfigs'
+
+let providersArray: ProvidersArray
+if (import.meta.env.VITE_ALGOD_NETWORK === '') {
+  const kmdConfig = getKmdConfigFromViteEnvironment()
+  providersArray = [
+    {
+      id: PROVIDER_ID.KMD,
+      clientOptions: {
+        wallet: kmdConfig.wallet,
+        password: kmdConfig.password,
+        host: kmdConfig.server,
+        token: String(kmdConfig.token),
+        port: String(kmdConfig.port),
+      },
+    },
+  ]
+} else {
+  providersArray = [
+    { id: PROVIDER_ID.DEFLY, clientStatic: DeflyWalletConnect },
+    { id: PROVIDER_ID.PERA, clientStatic: PeraWalletConnect },
+    { id: PROVIDER_ID.DAFFI, clientStatic: DaffiWalletConnect },
+    { id: PROVIDER_ID.EXODUS },
+    // If you are interested in WalletConnect v2 provider
+    // refer to https://github.com/TxnLab/use-wallet for detailed integration instructions
+  ]
+}
 
 export default function App() {
   const [openWalletModal, setOpenWalletModal] = useState<boolean>(false)
@@ -21,17 +50,20 @@ export default function App() {
 
   const algodConfig = getAlgodConfigFromViteEnvironment()
 
-  const walletProviders = useAlgoWallet({
-    nodeToken: String(algodConfig.token),
-    nodeServer: algodConfig.server,
-    nodePort: String(algodConfig.port),
-    network: algodConfig.network,
-    autoConnect: true,
+  const walletProviders = useInitializeProviders({
+    providers: providersArray,
+    nodeConfig: {
+      network: algodConfig.network,
+      nodeServer: algodConfig.server,
+      nodePort: String(algodConfig.port),
+      nodeToken: String(algodConfig.token),
+    },
+    algosdkStatic: algosdk,
   })
 
   return (
     <SnackbarProvider maxSnack={3}>
-      <WalletProvider value={walletProviders.walletProviders}>
+      <WalletProvider value={walletProviders}>
         <div className="hero min-h-screen bg-teal-400">
           <div className="hero-content text-center rounded-lg p-6 max-w-md bg-white mx-auto">
             <div className="max-w-md">
