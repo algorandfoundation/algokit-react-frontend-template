0897bb4aaae3424cf62e66c0a43929887470d70d
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-23 13:43:46 +1000

    chore: remove 'types' from algokit utils imports

--- template_content/src/interfaces/network.ts
+++ template_content/src/interfaces/network.ts
@@ -1,4 +1,4 @@
-import { AlgoClientConfig } from '@algorandfoundation/algokit-utils/types/network-client'
+import { AlgoClientConfig } from '@algorandfoundation/algokit-utils/network-client'
 
 export interface AlgoViteClientConfig extends AlgoClientConfig {
   /** Base URL of the server e.g. http://localhost, https://testnet-api.algonode.cloud/, etc. */
