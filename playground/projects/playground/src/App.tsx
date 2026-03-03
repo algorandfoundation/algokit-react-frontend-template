bfca6cf57944b8ea23ba1b1b1997b0b2e1d50125
p2arthur <arthurrabelopro@gmail.com>
2026-03-03 07:37:13 -0800

    feat: start adding web3auth to react template

--- playground/projects/playground/src/App.tsx
+++ playground/projects/playground/src/App.tsx
@@ -21,10 +21,6 @@ if (import.meta.env.VITE_ALGOD_NETWORK === 'localnet') {
     { id: WalletId.DEFLY },
     { id: WalletId.PERA },
     { id: WalletId.EXODUS },
-    {
-      id: WalletId.WEB3AUTH,
-      options: { clientId: import.meta.env.VITE_WEB3AUTH_CLIENT_ID, web3AuthNetwork: import.meta.env.VITE_WEB3AUTH_NETWORK },
-    },
     // If you are interested in WalletConnect v2 provider
     // refer to https://github.com/TxnLab/use-wallet for detailed integration instructions
   ]
