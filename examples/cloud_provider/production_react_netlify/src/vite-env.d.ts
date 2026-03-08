c5abe59ed6b3ccedea459c7020704b4e06c2b662
p2arthur <arthurrabelopro@gmail.com>
2026-03-08 13:55:52 -0700

    chore: update generated templates

--- examples/cloud_provider/production_react_netlify/src/vite-env.d.ts
+++ examples/cloud_provider/production_react_netlify/src/vite-env.d.ts
@@ -17,8 +17,6 @@ interface ImportMetaEnv {
   readonly VITE_KMD_PORT: string
   readonly VITE_KMD_PASSWORD: string
   readonly VITE_KMD_WALLET: string
-
-  readonly VITE_WEB3AUTH_CLIENT_ID: string
 }
 
 interface ImportMeta {
